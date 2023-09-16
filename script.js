91b20eed0b8c668a5ebcc2a6cfe72e6f8e9e238c
Subham jain <134865513+Subham7769@users.noreply.github.com>
2023-09-17 00:09:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+function info(){
+    document.getElementById("browser-info").innerHTML = "You are using " + navigator.appName + " version " + navigator.appVersion;
+
+};
